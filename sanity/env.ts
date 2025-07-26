afe958fef10d18f83a8e91a535c0ef5c49f08738
mcwachira <mcwachira@outlook.com>
2025-07-26 10:02:36 +0300

    made changes to my sanity to make sanity studio work

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,17 +1,23 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-07-25";
 
+console.log("TEST VAR:", process.env.NEXT_PUBLIC_TEST);
+console.log(
+  "All NEXT_PUBLIC vars:",
+  Object.keys(process.env).filter((key) => key.startsWith("NEXT_PUBLIC_")),
+);
+
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
+  process.env.SANITY_STUDIO_DATASET,
+  "Missing environment variable: SANITY_STUDIO_DATASET",
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+  process.env.SANITY_STUDIO_PROJECT_ID, // Changed this
+  "Missing environment variable: SANITY_STUDIO_PROJECT_ID",
 );
 
-export const useCdn = true;
+// export const useCdn = true;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
